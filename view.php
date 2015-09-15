9b0e794420be25457fffb47e9840cec3fdf14daf
Simone Cattaneo <simone@neo2it.net>
2015-09-15 19:00:10 +0200

    Improved edit mode detection for stacks

--- view.php
+++ view.php
@@ -1,17 +1,10 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.");
 
-$isDashboard = false;
-if (Page::getCurrentPage()->getCollectionPath() == "/dashboard/blocks/stacks") {
-$isDashboard = true;
-} 
-
-$isStacks = false;
-if (strpos(Page::getCurrentPage()->getCollectionPath(), '!stacks') !== FALSE) {
-$isStacks = true;
-}
+$isStacks = ((Page::getCurrentPage()->getCollectionPath() == "/dashboard/blocks/stacks") ||
+			(strpos(Page::getCurrentPage()->getCollectionPath(), '!stacks') !== FALSE));
 
 $c=Page::getCurrentPage();
-if ($c->isEditMode() || $isDashboard || $isStacks) {
+if ($c->isEditMode() || $isStacks) {
 	if ($visible != '1') {
 		echo '<div style="background:#f00;color:#fff;text-align:center;">*** Content not visible in production ***</div>';
 	}
